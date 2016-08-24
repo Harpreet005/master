90967cdf08c65ed136daa972e7649db47aa5af38
goldyhmr237 <arvind.softweaver@gmail.com>
2016-08-24 11:12:37 +0530

    First commit

---
+++ platforms/android/assets/www/img/loadmore/config.php
@@ -0,0 +1,13 @@
+<?php
+//Database Configuration
+$hostname = "localhost";
+$user = "root";
+$password = "softweaver";
+$database = "codebos";
+$prefix = "";
+//Loadmore configuarion
+$resultsPerPage=5;
+
+$con = mysqli_connect($hostname,$user, $password,$database);
+
+?>
